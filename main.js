2f981b60198cbf74bad9273d600b2d03fc110333
Ummi Safiyya <UMMI_SAFIYYA_BINTE_SYED_FEROZ@MAI.SG>
2019-05-25 15:59:20 +0800

    link button to pages
    linking the bypages and bydays buttons the their respective pages

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 function calculateDaysNeeded() {
-
     const totalPages = 604;
     const inputValue = document.getElementById("number").value;
-    const inputNumber = parseInt(inputValue);
 
-    const answer = totalPages/inputNumber
-    document.getElementById("results").innerHTML = answer;
-
-    
+    const answer = totalPages/inputValue
+    document.getElementById("results1").innerHTML = answer;
 }
+
+function calculatePagesNeeded() {
+    const totalPages = 604
+    const inputDays = document.getElementById("days").value
+
+    const answer = totalPages/inputDays
+    document.getElementById("results2").innerHTML = answer
+
+
+}
+ 
